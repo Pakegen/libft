cee9edbc699d6d8940f981cb7a4fe7001acdb525
Lanila <109435712+Pakegen@users.noreply.github.com>
2024-05-22 23:20:21 +0200

    Add files via upload

---
+++ ft_memcmp.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: qacjl <qacjl@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/22 23:07:49 by qacjl             #+#    #+#             */
+/*   Updated: 2024/05/22 23:18:19 by qacjl            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include"libft.h"
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	unsigned char	*p1;
+	unsigned char	*p2;
+
+	p1 = (unsigned char *)s1;
+	p2 = (unsigned char *)s2;
+	while(n > 0)
+	{
+		if (p1 != p2)
+			return (p1 - p2);
+		p1++;
+		p2++;
+		n--;
+	}
+	return (NULL);
+}
